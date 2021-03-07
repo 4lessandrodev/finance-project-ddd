26d61297338856e9cda614901b3ef18d2a8ef886
Alessandrodev <alessandroadm@live.com>
2021-03-07 20:48:39 -0300

    test: ensure create a valid  password.value-object

--- domain/user/values-objects/password/password.value-object.spec.ts
+++ domain/user/values-objects/password/password.value-object.spec.ts
@@ -1,18 +1,25 @@
 import { PasswordValueObject } from './password.value-object';
 
 describe('password.value-object', () => {
-  it('should return a valid password', () => {
+  it('should create a valid password', () => {
     const password = PasswordValueObject.create('123abc');
+
     expect(password.isSuccess).toBe(true);
     expect(password.getResult().value).toBe('123abc');
   });
 
-  it('should return fail if provide a password greatter than 15 char', () => {
-    const password = PasswordValueObject.create(
-      'invalid_password_greatter_than_15_char',
+  it('should fail if password is not on range min 3 char and max 20 char', () => {
+    const shortPassword = PasswordValueObject.create('i');
+    expect(shortPassword.isFailure).toBe(true);
+    expect(shortPassword.error).toBe(
+      'Password must have min 3 char and max 20 char',
     );
-
-    expect(password.isFailure).toBe(true);
-    expect(password.error).toBe('Password max length 15 char');
+    const longPassword = PasswordValueObject.create(
+      'invalid_long_password_to_validate_password_must_have_max_20_char',
+    );
+    expect(longPassword.isFailure).toBe(true);
+    expect(longPassword.error).toBe(
+      'Password must have min 3 char and max 20 char',
+    );
   });
 });
