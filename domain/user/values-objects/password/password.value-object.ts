90b93ae711ef41ede28460808f64f0f33e2c7c5b
Alessandrodev <alessandroadm@live.com>
2021-03-07 20:48:13 -0300

    feat: create password.value-object

--- domain/user/values-objects/password/password.value-object.ts
+++ domain/user/values-objects/password/password.value-object.ts
@@ -3,7 +3,6 @@ import { Result, ValueObject } from '../../../shared';
 export interface PasswordValueObjectProps {
   value: string;
 }
-
 export class PasswordValueObject extends ValueObject<PasswordValueObjectProps> {
   private constructor(props: PasswordValueObjectProps) {
     super(props);
@@ -14,10 +13,12 @@ export class PasswordValueObject extends ValueObject<PasswordValueObjectProps> {
   }
 
   public static create(password: string): Result<PasswordValueObject> {
-    const isValidLength = password.length < 15;
+    const isValidPasswordLength = password.length >= 3 && password.length <= 20;
 
-    if (!isValidLength) {
-      return Result.fail<PasswordValueObject>('Password max length 15 char');
+    if (!isValidPasswordLength) {
+      return Result.fail<PasswordValueObject>(
+        'Password must have min 3 char and max 20 char',
+      );
     }
 
     return Result.ok<PasswordValueObject>(
