fb961045e52f281d81b2c3acc0f9423cd37ae0ed
Alessandrodev <alessandroadm@live.com>
2021-03-14 15:08:41 -0300

    feat: add userId.valueObject

---
+++ domain/user/value-objects/userId/userId.value-object.ts
@@ -0,0 +1,15 @@
+import { Entity, Result, UniqueEntityID } from '../../../shared';
+
+export class UserIdValueObject extends Entity<any> {
+  private constructor(id?: UniqueEntityID) {
+    super(null, id);
+  }
+
+  get id(): UniqueEntityID {
+    return this._id;
+  }
+
+  public static create(id?: UniqueEntityID): Result<UserIdValueObject> {
+    return Result.ok<UserIdValueObject>(new UserIdValueObject(id));
+  }
+}
