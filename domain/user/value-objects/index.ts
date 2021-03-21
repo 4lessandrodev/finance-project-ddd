753c801167062f527f3c552cd4206baa73aae656
Alessandrodev <alessandroadm@live.com>
2021-03-20 21:25:04 -0300

    feat: create index to export all value object and aggregate

---
+++ domain/user/value-objects/index.ts
@@ -0,0 +1,6 @@
+export * from './accepted-at/accepted-at.value-object';
+export * from './email/email.value-object';
+export * from './ip/ip.value-object';
+export * from './password/password.value-object';
+export * from './term/term.value-object';
+export * from './userId/userId.value-object';
