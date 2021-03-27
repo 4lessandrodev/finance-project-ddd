326ec31e9a259fdf72df84347a396f5f641bcabd
Alessandrodev <alessandroadm@live.com>
2021-03-27 11:52:53 -0300

    test: add tests to ensure a valid reason-description value object

---
+++ domain/budget-box/value-objects/reason-description/reason-description.value-object.spec.ts
@@ -0,0 +1,36 @@
+import { ReasonDescriptionValueObject } from './reason-description.value-object';
+
+describe('description.value-object', () => {
+  it('should create a valid description value object', () => {
+    const description = ReasonDescriptionValueObject.create(
+      'valid_description',
+    );
+    expect(description.isSuccess).toBe(true);
+  });
+
+  it('should normalize description to lowercase', () => {
+    const description = ReasonDescriptionValueObject.create(
+      'VaLiD_DesCriPtiOn',
+    );
+    expect(description.isSuccess).toBe(true);
+    expect(description.getResult().value).toBe('valid_description');
+  });
+
+  it('should fail if not provide description', () => {
+    const description = ReasonDescriptionValueObject.create(' ');
+    expect(description.isFailure).toBe(true);
+    expect(description.error).toBe(
+      'Invalid description lenght min 1 char and max 20 char',
+    );
+  });
+
+  it('should fail if provide long description (greatter than 20 char)', () => {
+    const description = ReasonDescriptionValueObject.create(
+      'Invalid description lenght greatter than max 20 char',
+    );
+    expect(description.isFailure).toBe(true);
+    expect(description.error).toBe(
+      'Invalid description lenght min 1 char and max 20 char',
+    );
+  });
+});
