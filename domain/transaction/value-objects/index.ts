ca97b285283932fd8bfb8191597ec91847525f77
Alessandrodev <alessandroadm@live.com>
2021-04-03 18:05:08 -0300

    feat: add index to export value objects

---
+++ domain/transaction/value-objects/index.ts
@@ -0,0 +1,5 @@
+export * from './attachment-path/attachment-path.value-object';
+export * from './transaction-calculations/transaction-calculations.value-object';
+export * from './transaction-note/transaction-note.value-object';
+export * from './transaction-status/transaction-status.value-object';
+export * from './transaction-type/transaction-type.value-object';
