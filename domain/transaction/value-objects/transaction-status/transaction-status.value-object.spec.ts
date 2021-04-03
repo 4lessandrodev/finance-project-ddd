63046895fb33d659da0b887d0ead8a82742ff3b6
Alessandrodev <alessandroadm@live.com>
2021-04-03 15:44:22 -0300

    test: change enum value on transaction status

--- domain/transaction/value-objects/transaction-status/transaction-status.value-object.spec.ts
+++ domain/transaction/value-objects/transaction-status/transaction-status.value-object.spec.ts
@@ -2,15 +2,16 @@ import { TransactionStatusValueObject } from './transaction-status.value-object'
 
 describe('transaction-status.value-object', () => {
   it('should create a valid transaction-status', () => {
-    const result = TransactionStatusValueObject.create('ENTRADA');
+    const result = TransactionStatusValueObject.create('CONCLUIDO');
 
     expect(result.isSuccess).toBe(true);
   });
 
   it('should create a valid transaction-status', () => {
-    const result = TransactionStatusValueObject.create('saida' as any);
+    const result = TransactionStatusValueObject.create('pendente' as any);
 
     expect(result.isSuccess).toBe(true);
+    expect(result.getResult().value).toBe('PENDENTE');
   });
 
   it('should fail if provide an invalid transaction-status as string', () => {
