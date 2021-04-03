84bb4757d0d6e1b25bc8ef592e24f9eb9622f125
Alessandrodev <alessandroadm@live.com>
2021-04-03 14:04:32 -0300

    test: add transaction type value object

--- domain/transaction/value-objects/transaction-type/transaction-type.value-object.spec.ts
+++ domain/transaction/value-objects/transaction-type/transaction-type.value-object.spec.ts
@@ -6,4 +6,11 @@ describe('transaction-type.value-object', () => {
 
     expect(result.isSuccess).toBe(true);
   });
+
+  it('should fail if provide an invalid transaction-type as string', () => {
+    const result = TransactionTypeValueObject.create('INVALID_TYPE' as any);
+
+    expect(result.isSuccess).toBe(false);
+    expect(result.error).toBe('Invalid option');
+  });
 });
