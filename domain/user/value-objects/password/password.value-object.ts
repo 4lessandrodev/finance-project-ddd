5a7072482cb107aa54e07597cc2014a47e321291
Alessandrodev <alessandroadm@live.com>
2021-04-03 18:38:58 -0300

    feat: add password encryption

--- domain/user/value-objects/password/password.value-object.ts
+++ domain/user/value-objects/password/password.value-object.ts
@@ -1,28 +1,53 @@
 import { Result, ValueObject } from '../../../shared';
+import { hashSync, compareSync } from 'bcrypt';
+const isEncryptPass = /\$2b\$\d\d\$[\s\S]{53}|{.}\b/gm;
 
 export interface PasswordValueObjectProps {
   value: string;
 }
 export class PasswordValueObject extends ValueObject<PasswordValueObjectProps> {
-  private constructor(props: PasswordValueObjectProps) {
+  private isEncrypted: boolean;
+  private constructor(props: PasswordValueObjectProps, isEncrypted: boolean) {
     super(props);
+    this.isEncrypted = isEncrypted;
   }
 
   get value(): string {
     return this.props.value;
   }
 
-  public static create(password: string): Result<PasswordValueObject> {
-    const isValidPasswordLength = password.length >= 3 && password.length <= 20;
+  isAlreadyEncrypt(): boolean {
+    return isEncryptPass.test(this.props.value);
+  }
 
-    if (!isValidPasswordLength) {
-      return Result.fail<PasswordValueObject>(
-        'Password must have min 3 char and max 20 char',
-      );
+  async encryptPassWord(): Promise<void> {
+    this.props.value = hashSync(this.props.value, 10);
+    this.isEncrypted = true;
+  }
+
+  async comparePasswords(plainText: string): Promise<boolean> {
+    if (this.isEncrypted) {
+      return compareSync(plainText, this.props.value);
+    }
+    return plainText === this.props.value;
+  }
+
+  public static create(password: string): Result<PasswordValueObject> {
+    const isEncrypt = isEncryptPass.test(password);
+
+    if (!isEncrypt) {
+      const isValidPasswordLength =
+        password.length >= 3 && password.length <= 20;
+
+      if (!isValidPasswordLength) {
+        return Result.fail<PasswordValueObject>(
+          'Password must have min 3 char and max 20 char',
+        );
+      }
     }
 
     return Result.ok<PasswordValueObject>(
-      new PasswordValueObject({ value: password }),
+      new PasswordValueObject({ value: password }, isEncrypt),
     );
   }
 }
