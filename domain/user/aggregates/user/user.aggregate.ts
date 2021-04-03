c65a193fd17c2e229fadd455c5589332ad96cbe5
Alessandrodev <alessandroadm@live.com>
2021-04-03 13:07:50 -0300

    refactor: change array of string to budgetbox id on user aggregate

--- domain/user/aggregates/user/user.aggregate.ts
+++ domain/user/aggregates/user/user.aggregate.ts
@@ -1,3 +1,4 @@
+import { BudgetIdValueObject } from '../../../budget-box/value-objects';
 import { AggregateRoot, Result, UniqueEntityID } from '../../../shared';
 import { EmailValueObject } from '../../value-objects/email/email.value-object';
 import { PasswordValueObject } from '../../value-objects/password/password.value-object';
@@ -6,7 +7,7 @@ import { TermValueObject } from '../../value-objects/term/term.value-object';
 export interface UserAggregateProps {
   email: EmailValueObject;
   password: PasswordValueObject;
-  budgetBoxIds?: string[];
+  budgetBoxIds?: BudgetIdValueObject[];
   totalBalanceAvaliable: number;
   terms: TermValueObject[];
 }
@@ -24,7 +25,7 @@ export class UserAggregate extends AggregateRoot<UserAggregateProps> {
     return this.props.password;
   }
 
-  get budgetBoxIds(): string[] {
+  get budgetBoxIds(): BudgetIdValueObject[] {
     return this.props.budgetBoxIds ?? [];
   }
 
