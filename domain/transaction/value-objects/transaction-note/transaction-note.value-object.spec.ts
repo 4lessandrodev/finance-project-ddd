6743d5fc240d9b15e0d9aecbf28059fb3d20d287
Alessandrodev <alessandroadm@live.com>
2021-04-03 15:43:10 -0300

    test: add transaction note value object

---
+++ domain/transaction/value-objects/transaction-note/transaction-note.value-object.spec.ts
@@ -0,0 +1,18 @@
+import { TransactionNoteValueObject } from './transaction-note.value-object';
+
+describe('transaction-note.value-object', () => {
+  it('should create a valid note', () => {
+    const note = TransactionNoteValueObject.create('One_valid_note');
+    expect(note.isSuccess).toBe(true);
+    expect(note.getResult().value).toBe('One_valid_note');
+  });
+
+  it('should fail if provide string greatter than 144 char', () => {
+    const note = TransactionNoteValueObject.create(`Sequi hic unde qui ut vero aut.
+    Enim nulla dicta asperiores.Eius dicta delectus natus in sit omnis eveniet ut sapiente.Magnam et magnam.
+    Consectetur ipsam minus rerum non.`);
+
+    expect(note.isSuccess).toBe(false);
+    expect(note.error).toBe('Note value should be less than 144 char');
+  });
+});
