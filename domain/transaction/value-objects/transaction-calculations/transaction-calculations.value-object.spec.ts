3d287ec552842a78cfce8949fe0e1bd7f7bf993d
Alessandrodev <alessandroadm@live.com>
2021-04-03 17:15:39 -0300

    test: add transaction calculation value object

---
+++ domain/transaction/value-objects/transaction-calculations/transaction-calculations.value-object.spec.ts
@@ -0,0 +1,30 @@
+import { BudgetIdValueObject } from '../../../budget-box/value-objects';
+import { UniqueEntityID } from '../../../shared';
+import { TransactionCalculationValueObject } from './transaction-calculations.value-object';
+
+describe('transaction-calculations.value-object', () => {
+  it('should create a valida calculation', () => {
+    const calculation = TransactionCalculationValueObject.create({
+      budgetBoxId: BudgetIdValueObject.create(
+        new UniqueEntityID('valid_budgetId'),
+      ).getResult(),
+      value: 200,
+    });
+    expect(calculation.isSuccess).toBe(true);
+    expect(calculation.getResult().calculation.value).toBe(200);
+    expect(calculation.getResult().calculation.budgetBoxId.id.toValue()).toBe(
+      'valid_budgetId',
+    );
+  });
+
+  it('should fail if provide a negative number', () => {
+    const calculation = TransactionCalculationValueObject.create({
+      budgetBoxId: BudgetIdValueObject.create(
+        new UniqueEntityID('valid_budgetId'),
+      ).getResult(),
+      value: -100,
+    });
+    expect(calculation.isSuccess).toBe(false);
+    expect(calculation.error).toBe('Value should be positive');
+  });
+});
