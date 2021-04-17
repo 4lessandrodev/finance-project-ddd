10105f9ea5be0e3a6737b8a86085f0725946d916
Alessandrodev <alessandroadm@live.com>
2021-04-17 17:56:16 -0300

    refactor(infra): change interface definition to generic abstract repository

--- src/infra/repositories/shared/core/interfaces/generic-repository.interface.ts
+++ src/infra/repositories/shared/core/interfaces/generic-repository.interface.ts
@@ -2,9 +2,12 @@ export interface FilterInterface {
   [key: string]: string | number;
 }
 
-export interface GenericRepositoryInterface<UserPersistence> {
-  save: () => Promise<void>;
+export interface GenericRepositoryInterface<
+  TargetPersistence,
+  DomainAggregate
+> {
+  save: (entity: TargetPersistence) => Promise<void>;
   delete: () => Promise<void>;
-  find: (filter: FilterInterface) => Promise<UserPersistence[]>;
+  find: (filter: FilterInterface) => Promise<DomainAggregate[]>;
   exist: (filter: FilterInterface) => Promise<boolean>;
 }
