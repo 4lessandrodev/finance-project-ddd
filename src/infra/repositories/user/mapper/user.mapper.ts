3fca9094c3e0a575fa7a1e5930ac28d816574412
Alessandrodev <alessandroadm@live.com>
2021-04-17 18:01:45 -0300

    feat(infra): implement user mapper

---
+++ src/infra/repositories/user/mapper/user.mapper.ts
@@ -0,0 +1,61 @@
+import { BudgetIdValueObject } from '@domain/budget-box/value-objects';
+import { MapperInterface } from '@infra/repositories/shared';
+import { UserPersistence as Entity } from '../interfaces/user-persistence.interface';
+import { UserAggregate } from '@domain/user/aggregates';
+import { UniqueEntityID } from '@domain/shared';
+import {
+  DateValueObject,
+  EmailValueObject,
+  IpValueObject,
+  PasswordValueObject,
+  TermValueObject,
+} from '@domain/user/value-objects';
+
+export class UserMapper implements MapperInterface<Entity, UserAggregate> {
+  //
+  toPersistence(target: UserAggregate): Entity {
+    return {
+      id: target.id.toString(),
+      email: target.email.value,
+      password: target.password.value,
+      terms: target.terms.map(({ terms }) => ({
+        acceptedAt: terms.acceptedAt.value.toISOString(),
+        ip: terms.ip.value,
+        userAgent: { ...terms.userAgent },
+      })),
+      totalBalanceAvaliable: target.totalBalanceAvaliable,
+      budgetBoxIds: target.budgetBoxIds.map(({ id }) => id.toString()),
+      isDeleted: target.isDeleted,
+      updatedAt: target.updatedAt.toISOString(),
+      createdAt: target.createdAt.toISOString(),
+      deletedAt: target.deletedAt ? target.deletedAt.toISOString() : undefined,
+    };
+  }
+  //
+  toDomain(target: Entity): UserAggregate {
+    return UserAggregate.create(
+      {
+        email: EmailValueObject.create(target.email).getResult(),
+        password: PasswordValueObject.create(target.password).getResult(),
+        terms: target.terms.map(({ acceptedAt, ip, userAgent }) =>
+          TermValueObject.create({
+            acceptedAt: DateValueObject.create(
+              new Date(acceptedAt),
+            ).getResult(),
+            ip: IpValueObject.create(ip).getResult(),
+            userAgent: userAgent as any,
+          }).getResult(),
+        ),
+        totalBalanceAvaliable: target.totalBalanceAvaliable,
+        budgetBoxIds: target.budgetBoxIds?.map((id) =>
+          BudgetIdValueObject.create(new UniqueEntityID(id)).getResult(),
+        ),
+        createdAt: new Date(target.createdAt),
+        isDeleted: target.isDeleted,
+        updatedAt: new Date(target.updatedAt),
+        deletedAt: target.deletedAt ? new Date(target.deletedAt) : undefined,
+      },
+      new UniqueEntityID(target.id),
+    ).getResult();
+  }
+}
