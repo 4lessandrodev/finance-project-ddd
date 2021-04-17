0775997f44a08fb36f76e1ed87add734d44375fc
Alessandrodev <alessandroadm@live.com>
2021-04-17 18:01:18 -0300

    feat(infra): implement user repository interface

--- src/infra/repositories/user/interfaces/user-repository.interface.ts
+++ src/infra/repositories/user/interfaces/user-repository.interface.ts
@@ -1,6 +1,6 @@
 import { GenericRepositoryInterface } from '@infra/repositories/shared';
 
-export interface UserRepositoryInterface<UserPersistence, ORM>
-  extends GenericRepositoryInterface<UserPersistence> {
+export interface UserRepositoryInterface<UserPersistence, DomainAggregate, ORM>
+  extends GenericRepositoryInterface<UserPersistence, DomainAggregate> {
   methods: () => ORM;
 }
