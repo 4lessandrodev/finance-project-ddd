ca01893e20495b3b0199a3ceeb3bce456a837338
Alessandrodev <alessandroadm@live.com>
2021-05-30 10:37:28 -0300

    feat: added database connection config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+	  MongooseModule.forRoot("mongodb://localhost/fiance_api")
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return '<h1>Hello World in html <h1/>';
-  }
-}
