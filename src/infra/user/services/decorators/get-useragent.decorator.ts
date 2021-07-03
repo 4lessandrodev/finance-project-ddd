66003524f698373330880e5f4010d0f00f8d542b
4lessandrodev <alessandroadm@live.com>
2021-07-03 14:28:01 -0300

    feat: added user agent version

--- src/infra/user/services/decorators/get-useragent.decorator.ts
+++ src/infra/user/services/decorators/get-useragent.decorator.ts
@@ -14,8 +14,8 @@ export const GetUserAgent = createParamDecorator((_data: any, ctx: ExecutionCont
 	return {
 		name: userAgent?.browser?.name ?? 'undefined',
 		os: userAgent?.os?.name?.toUpperCase() as any ?? 'undefined',
-		type: userAgent?.device?.type ?? 'undefined',
-		version: userAgent?.os?.version ?? 'undefined'
+		type: userAgent?.device?.type ?? 'browser',
+		version: userAgent?.browser?.version?.slice(0, 4) ?? '89.0'
 	};
 
 });
