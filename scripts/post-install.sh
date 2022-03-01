9d3daae2b0a0202b3c9dc64321f81062dae12747
4lessandrodev <alessandroadm@live.com>
2022-03-01 12:05:46 -0300

    chore(env): added racking to check environment

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,10 +3,10 @@
 # This file check environment and run a specific command for each one
 
 # Get node environment
-NODE_ENV="$(printenv | awk '/^NODE_ENV/{print $1}')";
+NODEJS_ENV="$(printenv | awk '/^NODE_ENV/{print $1}')";
 
 
-if [[ "$NODE_ENV" == "NODE_ENV=production" ]]; then
+if [[ "$NODEJS_ENV" == "NODE_ENV=production" ]]; then
 
 	# Run production commands
 	echo "Production";
