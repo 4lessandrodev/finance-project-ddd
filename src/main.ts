4d7a44870304c3ae9262ced7f866c55194690e71
4lessandrodev <alessandroadm@live.com>
2022-04-21 13:48:12 -0300

    test(proxy): added tests to proxy

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PORT } from '@config/env';
 
-async function bootstrap() {
+async function bootstrap () {
 	const app = await NestFactory.create(AppModule);
 	await app.listen(PORT);
 }
