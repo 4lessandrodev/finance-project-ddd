2c57b97c418c1e368067bffd55fb2e11263fb6d8
4lessandrodev <alessandroadm@live.com>
2022-05-17 04:22:17 -0300

    docs(voyaer): update documentation

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { PORT } from '@config/env';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import helmet from 'helmet';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 
 async function bootstrap () {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.setViewEngine('ejs');
 	app.setBaseViewsDir(join(__dirname, '..', 'views'));
 	app.useStaticAssets(join(__dirname, '..', 'public'));
+	app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
 	app.use(helmet());
 	await app.listen(process.env.PORT ?? PORT);
 }
