474f8da01f9c85ba6ab6e845aaa41573d735341d
4lessandrodev <alessandroadm@live.com>
2022-05-17 02:27:07 -0300

    feat(security): added helmet

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { AppModule } from './app.module';
 import { PORT } from '@config/env';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import helmet from 'helmet';
 
 async function bootstrap () {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 	app.setViewEngine('ejs');
 	app.setBaseViewsDir(join(__dirname, '..', 'views'));
 	app.useStaticAssets(join(__dirname, '..', 'public'));
+	app.use(helmet());
 	await app.listen(process.env.PORT ?? PORT);
 }
 bootstrap();
